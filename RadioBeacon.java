59a616ce0cdfbfde24df2556f891cbee50c2f419
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-01-14 10:10:40 -0800

    Command /ant to list antennas

--- RadioBeacon.java
+++ RadioBeacon.java
@@ -1,8 +1,10 @@
 
 package com.exphc.RadioBeacon;
 
+import java.util.Map;
+import java.util.Iterator;
 import java.util.logging.Logger;
-import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.*;
@@ -11,18 +13,22 @@ import org.bukkit.event.player.*;
 import org.bukkit.Material.*;
 import org.bukkit.block.*;
 import org.bukkit.entity.*;
+import org.bukkit.command.*;
 import org.bukkit.*;
 
 class Antenna implements Comparable {
     Location location;
     boolean enabled;
 
-    Logger log = Logger.getLogger("Minecraft");
+    static Logger log = Logger.getLogger("Minecraft");
+
+    static ConcurrentHashMap<Location, Antenna> ants = new ConcurrentHashMap<Location, Antenna>();
 
     public Antenna(Location loc) {
         location = loc;
         enabled = true;
 
+        ants.put(loc, this);
         log.info("New antenna at " + loc);
     }
 
@@ -43,8 +49,11 @@ class Antenna implements Comparable {
         Antenna rhs = (Antenna)obj;
 
         //return location.compareTo(rhs.location);
-
         return location.hashCode() - rhs.location.hashCode();
+    }
+
+    public String toString() {
+        return location.toString();
     }
 }
 
@@ -62,7 +71,7 @@ class BlockPlaceListener extends BlockListener {
 
         if (block.getType() == Material.IRON_BLOCK) {
             if (block.isBlockPowered() || block.isBlockIndirectlyPowered()) {
-                plugin.createAntenna(block.getLocation());
+                new Antenna(block.getLocation());
             }
 
             player.sendMessage("powered? " + block.isBlockPowered());
@@ -109,7 +118,6 @@ public class RadioBeacon extends JavaPlugin {
     BlockListener blockListener;
     PlayerListener playerListener;
 
-    ConcurrentSkipListSet<Antenna> ants = new ConcurrentSkipListSet<Antenna>();
 
     public void onEnable() {
 
@@ -122,7 +130,6 @@ public class RadioBeacon extends JavaPlugin {
         getServer().getPluginManager().registerEvent(org.bukkit.event.Event.Type.PLAYER_INTERACT, playerListener, org.bukkit.event.Event.Priority.Lowest, this);
 
 
-
         log.info("beacon enable");
     }
 
@@ -130,12 +137,20 @@ public class RadioBeacon extends JavaPlugin {
         log.info("beacon disable");
     }
 
-    public Antenna createAntenna(Location loc)
-    {   
-        Antenna ant = new Antenna(loc);
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if (!cmd.getName().equalsIgnoreCase("ant")) {
+            return false;
+        }
 
-        ants.add(ant);
-        
-        return ant;
+        Iterator it = Antenna.ants.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            Location loc = (Location)pair.getKey();
+            Antenna ant = (Antenna)pair.getValue();
+
+            sender.sendMessage("Antenna at " + ant);
+        }
+
+        return true;
     }
 }
