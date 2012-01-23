82e01f3304cb404be01372f45d26886bedf28f71
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-01-23 08:48:13 -0800

    Update for 0.8 pre-release, include source

--- go.sh
+++ go.sh
@@ -3,5 +3,5 @@ CLASSPATH=../craftbukkit-1.0.1-R1.jar javac *.java
 rm -rf me 
 mkdir -p me/exphc/RadioBeacon
 mv *.class me/exphc/RadioBeacon
-jar cf RadioBeacon.jar me/ *.yml
+jar cf RadioBeacon.jar me/ *.yml *.java
 cp RadioBeacon.jar ../plugins/
