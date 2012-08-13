4f7fd6235645c4a9a2d75ffb8c459f15a84239e7
Kieran Wallbanks <1millionchances@gmail.com>
2012-08-13 18:24:50 +0100

    First commit

--- go.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -x
-CLASSPATH=../craftbukkit-1.2.5-R2.0.jar javac *.java -Xlint:deprecation -Xlint:unchecked
-rm -rf me 
-mkdir -p me/exphc/RadioBeacon
-mv *.class me/exphc/RadioBeacon
-jar cf RadioBeacon.jar me/ *.yml *.java LICENSE *.md
